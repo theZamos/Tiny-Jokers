7050658028e3727c13dd5d7966ca60283f74b40d
Zamos <54541594+theZamos@users.noreply.github.com>
2025-02-24 01:11:23 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,48 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Tiny Jokers
+--- MOD_ID: tiny_jokers
+--- PREFIX: tiny_jokers
+--- MOD_AUTHOR: [Zamos]
+--- MOD_DESCRIPTION: Turns the big Jimbos into tiny Timbos.
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture({
+    key = 'tinyJokers',
+    set = 'Joker',
+    path = 'tinyJokers.png',
+    display_pos = {x = 0, y = 9},
+    loc_txt = {
+      name = 'Tiny Jokers'
+    }
+})
+AltTexture({
+    key = 'tinyStickers',
+    set = 'Sticker',
+    path = 'tinyStickers.png',
+    loc_txt = {
+        name = 'Tiny Stickers'
+    }
+})
+AltTexture({
+    key = 'tinyStakes',
+    set = 'Stake',
+    path = 'tinyStakes.png',
+    stickers = true,
+    loc_txt = {
+        name = 'Tiny Stake Stickers'
+    }
+})
+
+TexturePack({
+    key = 'tinyTextures',
+    textures = {
+      'tiny_jokers_tinyJokers',
+      'tiny_jokers_tinyStickers',
+      'tiny_jokers_tinyStakes'
+    },
+    loc_txt = {
+      name = 'Tiny Jokers',
+      text = {'See these Jokers?', 'Very Tiny.', 'Very Demure.'}
+    }
+})
